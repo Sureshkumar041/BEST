efdee7f07b90b309e7b2a598b2e9dbda55441df0
Sureshkumar041 <sureshkumarbe04@gmail.com>
2022-11-07 21:47:50 +0530

    Add all my files

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,5 +1,6 @@
 var data = JSON.parse(localStorage.getItem("UserData"));
 var userid = localStorage.getItem("User Id");
+var index;
 
 data.forEach(element => {
     if(userid == element.id){
@@ -12,9 +13,8 @@ data.forEach(element => {
 // var options =[email,username,mobile,action];
 
 data.forEach(element => {
-    var content = document.getElementById("table");
+    var content = document.getElementById("tbody");
     var newrow = content.insertRow();
-
     //  Print Register Data
     var cell = newrow.insertCell(0);
     cell.innerHTML = element.id;
@@ -28,18 +28,21 @@ data.forEach(element => {
     cell.innerHTML = '<button class="btn btn-primary" onclick="onEdit(this)">Edit</button> <button class="btn btn-danger" onClick="onDelete(this)">Delete</button>';
 });
 
-onShow = () => document.getElementById("table").style.visibility = "visible";
+onShow = () =>{
+    // document.getElementsByClassName("pagination").style.display = "block";
+    document.getElementById("table").style.visibility = "visible";
+} 
 
 onHidden = () => document.getElementById("action").style.display = "none";
 
 onEdit = (row) => {
-    document.getElementById("divtable").style.display = "none";
+    // document.getElementById("divtable").style.display = "none";
     document.getElementById("action").style.display = "block";
-
     var data = JSON.parse(localStorage.getItem("UserData"));
-    var index = row.parentElement.parentElement.rowIndex;
+    index = row.parentElement.parentElement.rowIndex;
+    console.log("Index: ",index);
+    // alert("Index");
     idinput = document.getElementById("table").rows[index].cells[0].innerHTML;
-
     data.forEach(element => {
         if (idinput == element.id) {
             console.log("Good");
@@ -115,7 +118,7 @@ onUpdate = () => {
                 element.email = emailupdate;
                 element.username = nameupdate;
                 element.mobileno = noupdate;
-                var changes = document.getElementById("table").rows[idinput].cells;
+                var changes = document.getElementById("table").rows[index].cells;
                 changes[0].innerHTML = element.id;
                 changes[1].innerHTML = element.email;
                 changes[2].innerHTML = element.username;
@@ -134,23 +137,25 @@ onUpdate = () => {
 onDelete=(row)=>{
     var data = JSON.parse(localStorage.getItem("UserData"));
     var index = row.parentElement.parentElement.rowIndex;
+    console.log("Index For Delete: ",index);
     // var deluser = 0;
     idinput = document.getElementById("table").rows[index].cells[0].innerHTML;
     console.log("User Id: ",idinput);
     if(confirm("Do you want to delete this record ?")){
         // Delete A data From Table
-        document.getElementById("table").deleteRow(idinput);
+        document.getElementById("table").deleteRow(index);
 
         var deluser = data.findIndex(i => i.id == idinput );
         //Delete a data from LocalStorage
         data.splice(deluser,1);
-        var autoid = 1; // User Id increment
-        data.forEach(element => {
-            element.id = autoid;
-            var changes = document.getElementById("table").rows[idinput].cells;
-            changes[0].innerHTML = element.id;
-            autoid++;
-        });
+
+        // var autoid = 1;  User Id increment
+        // data.forEach(element => {
+        //     element.id = autoid;
+        //     var changes = document.getElementById("table").rows[index].cells;
+        //     changes[0].innerHTML = element.id;
+        //     autoid++;
+        // });
         localStorage.setItem("UserData",JSON.stringify(data));
         console.log("Local Index: ",deluser);
     }
@@ -263,31 +268,153 @@ onSearchbar=()=>{
     document.getElementById("searchbar").style.display = "block";
 }
 
+// Pagination 
+var tbody = document.querySelector("tbody");
+// var insidetable = document.querySelector("tbody");
+var navi = document.querySelector(".pagination");
+var row = tbody.querySelectorAll("tr");
+console.log("Row: ",row);
 
-console.log("Page start");
-//  var paginationFilter=()=>{
-    // alert("Pagination");
-    var pagination = document.getElementById("pagination");
-    var ulList = document.createElement('ul');
-    var liList = document.createElement('li');
-    var pageLink = document.createElement('a');
-    
-    pagination.setAttribute("class", "pagination");
-    pageLink.setAttribute("class", "navlink");
-    pageLink.setAttribute("href", "#");
-    
-    pagination.appendChild(ulList);
-    ulList.appendChild(liList);
-    liList.appendChild(pageLink);
-    
-    // return pagination; 
-// };
+var dummy = [];
+var indexs = 1;
+var itemperpage = 3;
 
+row.forEach(element => {
+    dummy.push(element);
+});
+// alert("Dummy");
+console.log("DUMMY: ",dummy);
 
-// for(var i=0;Array.length;i++){
-//     if(em == Array.email && pswd == Array.password)[
-//         alert("Success")
-//     ]else{
-//         alert("Error")
-//     }
+// Displaying user data in table
+displaydata = (itemperpage) => {
+    var i =1;
+    tbody.innerHTML = "";
+    dummy.forEach(element => {
+        if (i <= itemperpage) {
+            console.log("Times");
+            tbody.appendChild(element);
+            i++;
+        }
+    });
+    var pagnum = navi.querySelectorAll(".list");
+    pagnum.forEach(element => element.remove());
+}
+displaydata(itemperpage);
+
+// Creating dynamic pagination
+dynamicpagination=(itemperpage)=>{
+    // alert("Dynamic Page");
+    var total_row = dummy.length;
+    console.log("ITEM PER: ",itemperpage);
+    console.log("Total length: ",total_row);
+    if(total_row <= itemperpage) {
+        console.log("IF part"); navi.style.display = "none";
+    }    
+    else{
+        // alert("Else part");
+        navi.style.display = "flex";
+        var no_page = Math.ceil(total_row/itemperpage);
+        for (let index = 1; index <= no_page; index++) {
+            // const element = array[index];
+            var li = document.createElement("li");
+            li.className ="list page-item";
+            var anchor = document.createElement("a");
+            anchor.href = "#";
+            anchor.className ="page-link";
+            anchor.innerHTML = index;
+            anchor.setAttribute("datapage",index);
+            li.appendChild(anchor);
+            navi.insertBefore(li,navi.querySelector(".next"));            
+        }
+    } 
+}
+dynamicpagination(itemperpage);
+
+var pageconnect = document.querySelectorAll("a");
+console.log("A tag: ",pageconnect);
+var lastpage = pageconnect.length-2;
+
+// Connecting the page to another page
+pageConnector=(pageconnect,itemperpage,lastpage,pagehref)=>{
+    for(button of pageconnect){
+        button.onclick=e=>{
+            console.log("1");
+            var page_no = e.target.getAttribute("datapage");
+            console.log("Page no: ",page_no);
+            var pagemove = e.target.getAttribute("id");
+            console.log("Pagemove: ",pagemove);
+            console.log("MAIN Indexs: ",indexs);
+            if( page_no != null){
+                indexs = page_no;
+            }else{
+                if( pagemove === "next"){
+                    indexs++;
+                    if(indexs >= lastpage){
+                        indexs = lastpage;
+                    }
+                }else{
+                    indexs--;
+                    if(indexs <= 1){
+                        indexs = 1;
+                    }
+                }
+            }
+            pageCreate(indexs,itemperpage,pagehref);
+        }
+    }
+}
+
+var pagehref = navi.querySelectorAll(".list");
+console.log("PAGEHREF: ",pagehref);
+pagehref[0].classList.add("active");
+pageConnector(pageconnect,itemperpage,lastpage,pagehref);
+
+// giveValue=(itemperpage)=>{
+//     var pagehref = navi.querySelectorAll(".list");
+//     pagehref[0].classList.add("active");
+//     var pagelink = navi.querySelectorAll(".a");
+//     var lastpage = pagelink.length-2;
+//     console.log("Lastpage no: ",lastpage);
+//     pageConnector(pagelink,itemperpage,lastpage,pagehref); 
 // }
+
+pageCreate=(indexs,itemperpage,pagehref)=>{
+    var first = itemperpage*indexs;
+    var final = first + itemperpage;
+    var current_page = dummy.slice((first-itemperpage),(final - itemperpage));
+    console.log("Dummy Array: ",dummy);
+    console.log("Current Page: ",current_page);
+    tbody.innerHTML ="";
+    current_page.forEach(element => {
+        var u =0;
+        console.log(u++);
+        console.log("Element: ",element);
+        tbody.appendChild(element);
+    });
+    Array.from(pagehref).forEach((e)=>{
+        e.classList.remove("active");
+    });
+    console.log("I: ",indexs);
+    pagehref[indexs -1].classList.add("active");
+    console.log("Active Page: ",pagehref);
+}
+
+console.log("Dummy: ",dummy[0].cells[0]);
+onSearch=()=>{
+    var input, filter, table, tr, td, i, txtValue;
+    console.log("Data: ",data);
+    filter = document.getElementById("emailsearch").value.toUpperCase();
+    tbody.innerHTML ="";
+    for (i = 0; i < dummy.length; i++) {
+    td = dummy[i].cells[1];
+      if (td) {
+        txtValue = td.textContent || td.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        tbody.appendChild(dummy[i]);
+        } else {
+            dummy[i].style.display = "none";
+        }
+      }       
+    }
+  }
+  
